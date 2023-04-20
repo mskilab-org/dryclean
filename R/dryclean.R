45cf4bfbb8a2d0af238122ea1ebf9310f597ffdd
pblaney <patrick.blaney@nyulangone.org>
2023-04-20 14:50:30 -0400

    rework of merging inferred germline status for filter

--- R/dryclean.R
+++ R/dryclean.R
@@ -819,7 +819,7 @@ start_wash_cycle <- function(cov, mc.cores = 1, detergent.pon.path = NA, verbose
         # Let's add a proper merging of the 'germline.status'
         germ.file = rpca.1$inf_germ
         germ.file.dt = gUtils::gr2dt(germ.file)
-        cov_with_germline_status = merge(cov, germ.file.dt, all.x = T)
+        #cov_with_germline_status = merge(cov, germ.file.dt)
         #cov$germline.status = cov_with_germline_status$germline.status
 
         # 
@@ -835,7 +835,7 @@ start_wash_cycle <- function(cov, mc.cores = 1, detergent.pon.path = NA, verbose
     if (is.chr){
         cov = gr.chr(cov)
     }
-    return(cov_with_germline_status)
+    return(germ.file.dt)
 }
 
 message("Giddy up 4!")
